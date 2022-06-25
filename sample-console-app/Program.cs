d7f7b2f905ee26bcc1cf2a7046334391de6c1d6c
Jithin Naik <jithindnaik@gmail.com>
2022-06-25 18:00:50 +0530

    Sum , Finding the avg, For Loop

--- sample-console-app/Program.cs
+++ sample-console-app/Program.cs
@@ -1,8 +1,51 @@
-﻿namespace CSharpDotNetCore {
+﻿using System.Collections.Generic;
+
+namespace CSharpDotNetCore {
     public class SampleConsoleApp {
         public static void Main(string[] args) {
             // Console.WriteLine("Hello " + args[0] + ", Welcome to CSharp!!");
+            if (args.Length > 0) {
             Console.WriteLine($"Hello {args[0]}, Welcome to CSharp!!");
+            } else {
+                Console.WriteLine($"Hello User, Welcome to CSharp!!");
+            } 
+
+            int var1 = 10;
+            int var2 = 20;
+            int sum = var1 + var2;
+            Console.WriteLine(var1+var2);
+
+            double var11 = 10.123;
+            double var12 = 15.124;
+            Console.WriteLine(var11 + var12);
+
+            var listItems = new double[3];
+            listItems[0] = 10;
+            listItems[1] = 20;
+            listItems[2] = 30;
+
+            var sumList = listItems[0] + listItems[1] + listItems[2];
+            System.Console.WriteLine(sumList);
+
+            // var arrInit = new double[3] {10, 20, 30, 40, 50};
+            // var arrInit = new double[] {10, 20, 30, 40, 50};
+            var arrInit = new [] {10, 20, 30, 40, 50};
+            sumList = 0;
+            foreach(double num in arrInit) {
+                sumList+= num;
+            }
+            System.Console.WriteLine(sumList);
+
+            // List<double> grades = new List<double>();
+            var grades = new List<double>(){10.456, 20, 30};
+            grades.Add(100);
+            sumList = 0;
+            foreach(var num in grades) {
+                sumList+= num;
+            }
+            System.Console.WriteLine(sumList);
+            var len = grades.Count;
+            System.Console.WriteLine($"Avg grade is :: {sumList/len:N3}");
         }
     }
 }
---
+++ sample-grade-app/Program.cs
@@ -0,0 +1,31 @@
+﻿// Simple Sum Operation
+int num1 = 10;
+int num2 = 20;
+int num3 = 30;
+var sum = num1 + num2 + num3;
+System.Console.WriteLine(sum); 
+
+// Using a loop to take a list input and print the sum
+//int arr = new int[3];
+// var arr = new int[3] {10, 20, 30};
+var arr = new [] {10, 20, 30, 40, 50};
+var sum1 = 0;
+foreach(var x in arr) {
+    sum1 += x;
+}
+System.Console.WriteLine(sum1);
+
+// Using a list to take the list of input and print the sum
+// List<Double> grades = new List<Double> {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+var grades = new List<Double> {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+double gradeSum = 0;
+foreach(var x in grades) {
+    gradeSum += x;
+}
+System.Console.WriteLine(gradeSum);
+
+
+// Compute Avg of the grades 
+double avg = 0;
+avg = (gradeSum / grades.Count);
+System.Console.WriteLine($"Average of the grades :{avg:N2}"); // Avg grades to 2 decimal points
