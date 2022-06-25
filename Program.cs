b5d42e0ab6c649271a976183c336f271dcb7a0aa
jithindnaik <jithindnaik@gmail.com>
2022-06-25 15:01:54 +0530

    Initial Files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpDotNetCore {
+    public class SampleConsoleApp {
+        public static void Main(string[] args) {
+            // Console.WriteLine("Hello " + args[0] + ", Welcome to CSharp!!");
+            Console.WriteLine($"Hello {args[0]}, Welcome to CSharp!!");
+        }
+    }
+}
